b16442a03d3c805eb814f15c2c7587a78e94a663
Davy de Haas <davy.dehaas98@gmail.com>
2021-01-21 16:27:13 +0100

    Create Token.cs

---
+++ RepositoryPatternBase/Helpers/Token.cs
@@ -0,0 +1,58 @@
+﻿﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using PostMicroservice.Settings;
+
+namespace RepositoryPatternBase.Helpers
+{
+    public class Token : IToken
+    {
+        private readonly TokenSettings _tokenSettings;
+
+        public Token(IOptions<TokenSettings> tokenSettings)
+        {
+            _tokenSettings = tokenSettings.Value;
+        }
+
+        public string GenerateJwt(Guid userId, string email, string username)
+        {
+            var key = Encoding.ASCII.GetBytes(_tokenSettings.Secret);
+            var issuer = _tokenSettings.Issuer;
+            var audience = _tokenSettings.Audience;
+            
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                    new Claim(ClaimTypes.Email, email),
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Issuer = issuer,
+                Audience = audience,
+                IssuedAt = DateTime.UtcNow,
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        public string GetJwtClaim(string token, string claimType)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.ReadJwtToken(token.Replace("Bearer ", string.Empty));
+
+            return securityToken?.Claims.First(claim => claim.Type == claimType).Value;
+        }
+    }
+}
